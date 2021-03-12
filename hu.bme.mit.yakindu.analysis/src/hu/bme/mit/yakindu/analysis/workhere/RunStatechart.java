bcb099fe4823ea3f5fb03c81908183cca9d7d923
banko-marton <bankom@edu.bme.hu>
2021-03-12 07:07:07 +0100

    3.1-3.5

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,9 +1,10 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
@@ -11,25 +12,42 @@ import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
 public class RunStatechart {
 	
+	static ExampleStatemachine s;
+	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
-//		print(s);
-//		s.raiseStart();
-//		s.runCycle();
-//		System.in.read();
-//		s.raiseWhite();
-//		s.runCycle();
-//		print(s);
-//		System.exit(0);
+		s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		
+		s.runCycle();
+		
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			String command = sc.nextLine();
+			raiseEvent(command);
+			s.runCycle();
+			print(s);
+			if(command.equals("exit")) {
+					sc.close();
+					System.exit(0);
+			}
+		}
+	}
+	
+	private static void raiseEvent(String event) {
+		switch(event) {
+			case "start": s.raiseStart(); break;
+			case "white": s.raiseWhite(); break;
+			case "black": s.raiseBlack(); break;
+		}
 	}
 
 	public static void print(IExampleStatemachine s) {
 		System.out.println("W = " + s.getSCInterface().getWhiteTime());
 		System.out.println("B = " + s.getSCInterface().getBlackTime());
 	}
+	
+	
 }
--- hu.bme.mit.yakindu.analysis/yakindu-gen/hu/bme/mit/yakindu/analysis/example/ExampleStatemachine.java
+++ hu.bme.mit.yakindu.analysis/yakindu-gen/hu/bme/mit/yakindu/analysis/example/ExampleStatemachine.java
@@ -59,7 +59,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 		main_region_Init,
 		main_region_Black,
 		main_region_White,
-		main_region_Settings,
 		$NullState$
 	};
 	
@@ -118,9 +117,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 			case main_region_White:
 				main_region_White_react(true);
 				break;
-			case main_region_Settings:
-				main_region_Settings_react(true);
-				break;
 			default:
 				// $NullState$
 			}
@@ -174,8 +170,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 			return stateVector[0] == State.main_region_Black;
 		case main_region_White:
 			return stateVector[0] == State.main_region_White;
-		case main_region_Settings:
-			return stateVector[0] == State.main_region_Settings;
 		default:
 			return false;
 		}
@@ -277,12 +271,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 		stateVector[0] = State.main_region_White;
 	}
 	
-	/* 'default' enter sequence for state Settings */
-	private void enterSequence_main_region_Settings_default() {
-		nextStateIndex = 0;
-		stateVector[0] = State.main_region_Settings;
-	}
-	
 	/* 'default' enter sequence for region main region */
 	private void enterSequence_main_region_default() {
 		react_main_region__entry_Default();
@@ -310,12 +298,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 		exitAction_main_region_White();
 	}
 	
-	/* Default exit sequence for state Settings */
-	private void exitSequence_main_region_Settings() {
-		nextStateIndex = 0;
-		stateVector[0] = State.$NullState$;
-	}
-	
 	/* Default exit sequence for region main region */
 	private void exitSequence_main_region() {
 		switch (stateVector[0]) {
@@ -327,9 +309,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 			break;
 		case main_region_White:
 			exitSequence_main_region_White();
-			break;
-		case main_region_Settings:
-			exitSequence_main_region_Settings();
 			break;
 		default:
 			break;
@@ -354,12 +333,7 @@ public class ExampleStatemachine implements IExampleStatemachine {
 					exitSequence_main_region_Init();
 					enterSequence_main_region_White_default();
 				} else {
-					if (sCInterface.white) {
-						exitSequence_main_region_Init();
-						enterSequence_main_region_Settings_default();
-					} else {
-						did_transition = false;
-					}
+					did_transition = false;
 				}
 			}
 		}
@@ -407,17 +381,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 						did_transition = false;
 					}
 				}
-			}
-		}
-		return did_transition;
-	}
-	
-	private boolean main_region_Settings_react(boolean try_transition) {
-		boolean did_transition = try_transition;
-		
-		if (try_transition) {
-			if (react()==false) {
-				did_transition = false;
 			}
 		}
 		return did_transition;
